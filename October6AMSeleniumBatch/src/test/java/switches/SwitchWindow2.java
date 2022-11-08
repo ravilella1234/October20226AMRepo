565dc9004f84f3537be88ae3176d807037ed100d
ravilella1234 <masters.selenium@gmail.com>
2022-11-08 06:59:12 +0530

    Alerts Switchings

--- October6AMSeleniumBatch/src/test/java/switches/SwitchWindow2.java
+++ October6AMSeleniumBatch/src/test/java/switches/SwitchWindow2.java
@@ -1,6 +1,8 @@
 package switches;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -19,17 +21,25 @@ public class SwitchWindow2
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.icicibank.com");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 		System.out.println(driver.getTitle());
 		String parentWindow = driver.getWindowHandle();
 		System.out.println(parentWindow);
 		
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-		while(!wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//img[@alt='FD'][@src='/content/dam/icicibank/india/managed-assets/images/homepage/6.35-fd-rates-hp-d-cou.jpg'])[1]"))).isDisplayed())
+		while(!wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//div[@class='d-img image-media']/a/img[@alt='FD'])[2]"))).isDisplayed())
 		{
-			System.out.println(wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//img[@alt='FD'][@src='/content/dam/icicibank/india/managed-assets/images/homepage/6.35-fd-rates-hp-d-cou.jpg'])[1]"))).isDisplayed());
 			driver.findElement(By.xpath("(//button[@class='slick-next slick-arrow'])[1]")).click();
 		}
-		driver.findElement(By.xpath("(//img[@alt='FD'][@src='/content/dam/icicibank/india/managed-assets/images/homepage/6.35-fd-rates-hp-d-cou.jpg'])[1]")).click();
+		driver.findElement(By.xpath("(//div[@class='d-img image-media']/a/img[@alt='FD'])[2]")).click();
+		
+		Set<String> windows = driver.getWindowHandles();
+		ArrayList<String> nwinds = new ArrayList<String>(windows);
+		
+		for(String i:nwinds)
+		{
+			System.out.println(i);
+		}
 	}
 
 }
